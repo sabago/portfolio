7e69cba0aeda7581caad6b0246bb6c502e42593d
sabago <sandra.abago@gmail.com>
2020-10-11 19:38:19 -0400

    fixing routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {  BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {  HashRouter as Router, Switch, Route } from "react-router-dom";
 import { withRouter } from 'react-router-dom';
 
 // import About from "./About/About";
